25345d735bc996c05ea4b501c76c5633722024c9
nanacrackingthesecodes <dianakurochkina22@gmail.com>
2026-05-11 09:44:10 +0200

    222

--- src/components/Cat/Cat.js
+++ src/components/Cat/Cat.js
@@ -2,23 +2,89 @@ import "./Cat.css";
 
 function Cat() {
   const products = [
-    "/названабору1 copy.png","/названабору2 copy.png","/названабору3 copy.png",
-    "/названабору4 copy.jpg","/названабору5 copy.jpg","/названабору6 copy.jpg",
-    "/названабору7 copy.jpg","/названабору8 copy.png","/названабору9 copy.jpg",
-    "/названабору10 copy.png","/названабору11 copy.jpg","/названабору12 copy.jpg",
-    "/названабору13 copy.png","/названабору14 copy.png","/названабору15 copy.jpg",
-    "/названабору16 copy.jpg","/названабору17 copy.jpg","/названабору18 copy.jpg"
+    {
+      img: "/названабору1 copy.png",
+      title: "Nordvik",
+    },
+    {
+      img: "/названабору2 copy.png",
+      title: "Eira",
+    },
+    {
+      img: "/названабору3 copy.png",
+      title: "Fjord",
+    },
+    {
+      img: "/названабору4 copy.jpg",
+      title: "Lund",
+    },
+    {
+      img: "/названабору5 copy.jpg",
+      title: "Skala",
+    },
+    {
+      img: "/названабору6 copy.jpg",
+      title: "Oslo",
+    },
+    {
+      img: "/названабору7 copy.jpg",
+      title: "Bergen",
+    },
+    {
+      img: "/названабору8 copy.png",
+      title: "Malmö",
+    },
+    {
+      img: "/названабору9 copy.jpg",
+      title: "Sven",
+    },
+    {
+      img: "/названабору10 copy.png",
+      title: "Runa",
+    },
+    {
+      img: "/названабору11 copy.jpg",
+      title: "Oaken",
+    },
+    {
+      img: "/названабору12 copy.jpg",
+      title: "Arden",
+    },
+    {
+      img: "/названабору13 copy.png",
+      title: "Vega",
+    },
+    {
+      img: "/названабору14 copy.png",
+      title: "Lina",
+    },
+    {
+      img: "/названабору15 copy.jpg",
+      title: "Nora",
+    },
+    {
+      img: "/названабору16 copy.jpg",
+      title: "Stockholm",
+    },
+    {
+      img: "/названабору17 copy.jpg",
+      title: "Aster",
+    },
+    {
+      img: "/названабору18 copy.jpg",
+      title: "Nordic Moon",
+    },
   ];
 
   return (
     <section className="cat">
-      <h2>каталог</h2>
+      <h2>Каталог</h2>
 
       <div className="cat-grid">
-        {products.map((img, index) => (
+        {products.map((product, index) => (
           <div className="cat-item" key={index}>
-            <img src={img} alt="" />
-            <p>скандинавський набір</p>
+            <img src={product.img} alt={product.title} />
+            <p>{product.title}</p>
           </div>
         ))}
       </div>
